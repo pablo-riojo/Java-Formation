2466a6c3eb427f6cd7c3d3567467e15345130a74
[Pablo Riojo] <[pablo.riojo@bosonit.com]>
2022-11-15 20:20:17 +0100

    Returning row list

--- block1-process-file-and-streams/src/main/java/org/block1/Main.java
+++ block1-process-file-and-streams/src/main/java/org/block1/Main.java
@@ -8,32 +8,43 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Person> listPerson = new ArrayList<>();
-        Path myPath = Paths.get("C:\\Users\\pablo.riojo\\Desktop\\Java-Formation\\JF\\block1-process-file-and-streams\\Persons.csv");
-        readFile(myPath);
+        // ArrayList<Person> listPerson = new ArrayList<>();
+        Path myPath = Paths.get(
+                "C:\\Users\\pablo.riojo\\Desktop\\Java-Formation\\JF\\block1-process-file-and-streams\\Persons.csv");
+        List<String[]> dataRows = readFile(myPath);
     }
 
-    public static void readFile(Path path) {
+    public static List<String[]> readFile(Path path) {
         String line;
         String[] values;
+        List<String[]> rows = new ArrayList<>();
+
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path.toFile(), StandardCharsets.UTF_8));
 
             while ((line = reader.readLine()) != null) {
                 values = line.split(":");
-
-                for (int i = 0; i < values.length; i++) {
-                    System.out.println(values[i]);
-                }
+                rows.add(values);
             }
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return rows;
     }
+
+//    public static void cleanRows(String[] rows) {
+//        for (int i = 0; i < rows.length; i++) {
+//            System.out.println(rows.toString());
+//        }
+//    }
 }
+
