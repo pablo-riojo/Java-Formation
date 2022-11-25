b372db9c350d68d00017fc17850ee2be03509b7b
[Pablo Riojo] <[pablo.riojo@bosonit.com]>
2022-11-25 10:28:26 +0100

    block7-crud-validation-part2 UnprocessableEntityException

---
+++ block7-crud-validation/src/main/java/com/block7/block7crudvalidation/person/infrastructure/exception/unprocessableEntity/UnprocessableEntityException.java
@@ -0,0 +1,7 @@
+package com.block7.block7crudvalidation.person.infrastructure.exception.unprocessableEntity;
+
+public class UnprocessableEntityException  extends RuntimeException {
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+}
---
+++ block7-crud-validation/src/main/java/com/block7/block7crudvalidation/person/infrastructure/exception/unprocessableEntity/UnprocessableEntityHandler.java
@@ -0,0 +1,24 @@
+package com.block7.block7crudvalidation.person.infrastructure.exception.unprocessableEntity;
+
+import com.block7.block7crudvalidation.person.infrastructure.exception.CustomError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class UnprocessableEntityHandler {
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(value = {UnprocessableEntityException.class})
+    public ResponseEntity<Object> handleUnprocessableEntityException(UnprocessableEntityException e) {
+        HttpStatus unprocessableEntity = HttpStatus.UNPROCESSABLE_ENTITY;
+        CustomError customError = new CustomError(
+                e.getMessage(), unprocessableEntity.toString(), new Date()
+        );
+
+        return new ResponseEntity<>(customError, unprocessableEntity);
+    }
+}
