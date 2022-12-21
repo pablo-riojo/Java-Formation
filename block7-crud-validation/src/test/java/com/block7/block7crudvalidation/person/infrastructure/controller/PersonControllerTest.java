e3fd15332cb47b5babdc97990f6e72141d1e8bdf
[Pablo Riojo] <[pablo.riojo@bosonit.com]>
2022-12-21 10:43:20 +0100

    block7 fix PersonControllerTest

--- block7-crud-validation/src/test/java/com/block7/block7crudvalidation/person/infrastructure/controller/PersonControllerTest.java
+++ block7-crud-validation/src/test/java/com/block7/block7crudvalidation/person/infrastructure/controller/PersonControllerTest.java
@@ -3,10 +3,10 @@ package com.block7.block7crudvalidation.person.infrastructure.controller;
 import com.block7.block7crudvalidation.person.domain.Person;
 import com.block7.block7crudvalidation.person.infrastructure.dto.PersonInputDTO;
 import com.block7.block7crudvalidation.person.infrastructure.dto.PersonMapper;
-import com.block7.block7crudvalidation.person.infrastructure.dto.PersonOutputDTO;
 import com.block7.block7crudvalidation.person.infrastructure.repository.PersonRepository;
 import com.block7.block7crudvalidation.professor.application.ProfessorSvcImpl;
 import com.block7.block7crudvalidation.professor.domain.Professor;
+import com.block7.block7crudvalidation.professor.infrastructure.repository.ProfessorRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,9 +22,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 @AutoConfigureMockMvc(addFilters = false)
@@ -33,20 +30,19 @@ class PersonControllerTest {
     @Autowired private MockMvc mockMvc;
     @Autowired private ObjectMapper objectMapper;
     @Autowired private PersonRepository repository;
+    @Autowired private ProfessorRepository professorRepository;
     @Autowired private ProfessorSvcImpl professorSvc;
 
     private final UUID uuid = UUID.randomUUID();
-    private final Person person = new Person();
+    private Person person = new Person();
     private final Professor professor = new Professor();
-
     private static final PersonInputDTO personInputDTO = new PersonInputDTO();
-    private final PersonOutputDTO personOutputDTO = new PersonOutputDTO();
-    private final List<PersonOutputDTO> outputDTOList = new ArrayList<>();
 
     @BeforeEach
     void setUp() throws MalformedURLException {
         personInputDTO.setUser("PabloRDev");
         personInputDTO.setPassword("password");
+        personInputDTO.setAdmin(true);
         personInputDTO.setName("Pablo");
         personInputDTO.setSurname("Riojo");
         personInputDTO.setEmail("pablo@test.com");
@@ -56,10 +52,13 @@ class PersonControllerTest {
         personInputDTO.setActive(true);
 
         professor.setBranch("Frontend");
+
+       person = PersonMapper.Instance.personInputDTOToPerson(personInputDTO);
     }
 
     @AfterEach
     void tearDown() {
+        professorRepository.deleteAll();
         repository.deleteAll();
     }
 
@@ -89,7 +88,7 @@ class PersonControllerTest {
 
     @Test
     void getPersonById() throws Exception {
-        UUID id = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO)).getId();
+        UUID id = repository.save(person).getId();
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/person/" + id)
@@ -103,7 +102,7 @@ class PersonControllerTest {
 
     @Test
     void getPersonByUser() throws Exception {
-        String user = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO)).getUser();
+        String user = repository.save(person).getUser();
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/person/user/" + user)
@@ -117,7 +116,7 @@ class PersonControllerTest {
 
     @Test
     void getPersonByName() throws Exception {
-        String name = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO)).getName();
+        String name = personInputDTO.getName();
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/person/name/" + name)
@@ -131,7 +130,7 @@ class PersonControllerTest {
 
     @Test
     void getPersonBySurname() throws Exception {
-        String surname = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO)).getSurname();
+        String surname = personInputDTO.getSurname();
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/person/surname/" + surname)
@@ -145,15 +144,12 @@ class PersonControllerTest {
 
     @Test
     void getPersonByCreation() throws Exception {
-        Date date = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO)).getCreatedAt();
-
         RequestBuilder afterRequestBuilder = MockMvcRequestBuilders
                 .get("/person/creation?after=10/10/2010")
                 .accept(MediaType.APPLICATION_JSON);
         RequestBuilder beforeRequestBuilder = MockMvcRequestBuilders
                 .get("/person/creation?before=10/10/2010")
                 .accept(MediaType.APPLICATION_JSON);
-
 
         mockMvc.perform(afterRequestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -195,6 +191,7 @@ class PersonControllerTest {
     void updatePerson() throws Exception {
         Person person = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO));
         personInputDTO.setName("PRD");
+
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .put("/person/" + person.getId())
                 .accept(MediaType.APPLICATION_JSON)
@@ -208,9 +205,10 @@ class PersonControllerTest {
 
     @Test
     void deletePersonById() throws Exception {
-        Person person = repository.save(PersonMapper.Instance.personInputDTOToPerson(personInputDTO));
+        Person personDB = repository.save(person);
+
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .delete("/person/" + person.getId())
+                .delete("/person/" + personDB.getId())
                 .accept(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(requestBuilder)
